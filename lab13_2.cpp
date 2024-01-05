cb9815e0e906f83dcafdb36cff91d62e557fb016
Sensei <noppongsen@gmail.com>
2024-01-05 10:09:08 +0700

    lab13.2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,45 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool image[][M], int , int , int );
 
-void showImage(const bool [][M]);
+void showImage(const bool image[][M]);
 
-int main()
-{
+int main(){
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+
+    do {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
+
     return 0;
+}
+void updateImage(bool image[][M], int s, int x, int y) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            double distance = sqrt(((i - x) * (i - x)) + ((j - y) * (j - y)));
+            if (distance <= s-1) {
+                image[i][j] = 1;
+            }
+        }
+    }
+}
+void showImage(const bool image[][M]) {
+    cout << "------------------------------------------------------------------------\n";
+    for (int i = 0; i < N; ++i) {
+        cout << "|";
+        for (int j = 0; j < M; ++j) {
+            if (image[i][j] == 0) {
+                cout << " ";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------\n";
 }
